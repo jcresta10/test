132d343c88532b1bffdd3d2546a8abf8e57f354a
Jatin Shrestha <62802709+jcresta10@users.noreply.github.com>
2024-09-21 23:45:30 -0700

    Add files via upload

---
+++ scriptss.ps1
@@ -0,0 +1,22 @@
+﻿Write-Output "This is a benign script. Not malicious. Developed by Jatin Shrestha."
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jcresta10/test/main/benignscript.ps1" -OutFile "C:\Users\Public\benignscript.ps1"
+powershell -ExecutionPolicy Bypass -File "C:\Users\Public\benignscript.ps1"
+
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "MyBenignTask" -Value "C:\Windows\System32\notepad.exe" -PropertyType String
+
+Get-WinEvent -LogName "Microsoft-Windows-Sysmon/Operational" | 
+Where-Object { $_.Id -eq 3 -and $_.Message -like '*powershell.exe*' }
+
+Get-WinEvent -LogName "Microsoft-Windows-Sysmon/Operational" | 
+Where-Object { $_.Id -eq 1 } | 
+Select-Object -First 10
+
+
+Get-WinEvent -LogName "Microsoft-Windows-Sysmon/Operational" | 
+Where-Object { $_.Id -eq 3 } | 
+Select-Object -First 10
+
+Get-WinEvent -LogName "Microsoft-Windows-Sysmon/Operational" | 
+Where-Object { $_.Id -eq 13 } | 
+Select-Object -First 10
